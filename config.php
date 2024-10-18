80652018776b7d388ab4e87eb19ff30fd31ae5b3
harlemshake3 <harun.kahraman@hotmail.ch>
2024-10-18 14:33:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,18 @@
 <?php
 
-// Datenbankverbindungsparameter
 $host = '4c48df.myd.infomaniak.com';
 $dbname = '4c48df_IM3';
 $username = '4c48df_harunlol';
 $password = 'Lucatim1.';
 
-// DSN (Datenquellenname) für PDO
+
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
-// Optionen für PDO
+
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Aktiviert die Ausnahmebehandlung für Datenbankfehler
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Legt den Standard-Abrufmodus auf assoziatives Array fest
-    PDO::ATTR_EMULATE_PREPARES => false, // Deaktiviert die Emulation vorbereiteter Anweisungen, für bessere Leistung
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    PDO::ATTR_EMULATE_PREPARES => false, 
 ];
 
 ?>
